0207e0375f0133eab5586772ae3a56c578286e1a
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-19 22:56:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,15 @@
 class Dog 
   
   def name=(dogs_name)
-    your_dog_name = dogs_name
+    @your_dog_name = dogs_name
   end
   
   def name
-    your_dog_name 
+    @your_dog_name 
   end
   
  def bark
-    puts "Woof!"
+    puts "woof!"
   end
 end 
   
